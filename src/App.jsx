8dc6f9db01bc3beeb676f99f47346c983c2fe7dd
Coutrinlu20 <amarallua20@gmail.com>
2026-01-06 18:00:00 -0300

    script novo

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
    <main>
     <header>
-      <img src="/logo.png" />
+      <img src="/logo.png" alt=""/>
     </header>
      <section>
         <img src="/banner.png"  alt="" />
